7804fab0d965662998727cf9d46f1359b76e75a7
WebAdmin09 <husanorifjonov9@gmail.com>
2023-09-28 16:14:26 +0500

    dsvds

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,46 @@
-
+import style from './RegisterPage.module.css'
 const RegisterPage = () => {
     return (
-        <div>
-
-        </div>
+        <section>
+            <div className="container">
+                <div className={style["wrapper"]}>
+                    <h1 className={style["heading__login"]}>Login</h1>
+                    <form className={style["form"]}>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="First Name"
+                            className={style["form-input"]}
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Last Name"
+                            className={style["form-input"]}
+                        />
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            className={style["form-input"]}
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            className={style["form-input"]}
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Confirm password"
+                            className={style["form-input"]}
+                        />
+                        <button className={style["btn-login"]}>Register</button>
+                    </form>
+                </div>
+            </div>
+        </section>
     )
 }
 
